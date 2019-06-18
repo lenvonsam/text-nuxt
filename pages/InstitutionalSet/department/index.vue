63d904005a2ea806c0e54a66cc8f43ef899ccdb9
zhjm <943257156@qq.com>
2019-06-18 10:05:31 +0800

    test

--- pages/InstitutionalSet/department/index.vue
+++ pages/InstitutionalSet/department/index.vue
@@ -1,29 +1,29 @@
 <template lang="pug">
-div test
-//- erplr-panel(:right-padding="false")
-//-   div(slot="left")
-//-     left-search(:formItem="searchFormItems", :searchEvent="searchForm", ref="search")
-//-   .erp-content(slot="right")
-//-     button-group(:btns="buttonGroupInfo", @groupBtnClick="buttonGroupClick")
-//-     basic-elx-table(
-//-       :tableValue="tableValue",
-//-       :total="totalCount",
-//-       :currentPage="currentPage",
-//-       :pageSize="pageSize",     
-//-       @paginateChange="tableChange",
-//-       @tableRowEdit="rowEdit",
-//-       @tableRowDel="rowDel",
-//-       @rowSelection="rowSelection")
-//-     el-dialog(ref="dialog", :title="dialogTitle", :visible="dialogShow",  width="800px", @close="dialogHandler('cancel')")
-//-       el-form(v-if="dialogShow", ref="dialogForm", :model="smsTemplate", label-width="70px")
-//-         el-form-item(v-for="item in editForm", :label="item.lbl", :key="item.prop")
-//-           template(v-if="item.type === 'textarea'")
-//-             el-input(type="textarea", :rows="2", placeholder="请输入内容", v-model="smsTemplate[item.prop]", size="small")          
-//-           template(v-else)
-//-             el-input.full-width(v-model="smsTemplate[item.prop]", clearable, size="small", :placeholder="item.placeholder")
-//-       .dialog-footer(slot="footer")
-//-         el-button(@click="dialogHandler('cancel')", size="small") 取消
-//-         el-button(@click="dialogHandler('sure')", type="primary", size="small") 确定
+//- div test
+erplr-panel(:right-padding="false")
+  div(slot="left")
+    left-search(:formItem="searchFormItems", :searchEvent="searchForm", ref="search")
+  //- .erp-content(slot="right")
+    button-group(:btns="buttonGroupInfo", @groupBtnClick="buttonGroupClick")
+    basic-elx-table(
+      :tableValue="tableValue",
+      :total="totalCount",
+      :currentPage="currentPage",
+      :pageSize="pageSize",     
+      @paginateChange="tableChange",
+      @tableRowEdit="rowEdit",
+      @tableRowDel="rowDel",
+      @rowSelection="rowSelection")
+    el-dialog(ref="dialog", :title="dialogTitle", :visible="dialogShow",  width="800px", @close="dialogHandler('cancel')")
+      el-form(v-if="dialogShow", ref="dialogForm", :model="smsTemplate", label-width="70px")
+        el-form-item(v-for="item in editForm", :label="item.lbl", :key="item.prop")
+          template(v-if="item.type === 'textarea'")
+            el-input(type="textarea", :rows="2", placeholder="请输入内容", v-model="smsTemplate[item.prop]", size="small")          
+          template(v-else)
+            el-input.full-width(v-model="smsTemplate[item.prop]", clearable, size="small", :placeholder="item.placeholder")
+      .dialog-footer(slot="footer")
+        el-button(@click="dialogHandler('cancel')", size="small") 取消
+        el-button(@click="dialogHandler('sure')", type="primary", size="small") 确定
 </template>
 
 <script>
@@ -31,15 +31,15 @@ div test
   // import buttonGroup from '~/components/buttonGroup.vue'
   // import erplrPanel from '~/components/erplrPanel'
   // import basicElxTable from '~/components/basicElxTable'
-  // import leftSearch from '~/components/leftSearch'
+  import leftSearch from '~/components/leftSearch'
   export default {
     layout: 'backend',
-    // components: {      
-    //   basicElxTable,
-    //   erplrPanel,
-    //   leftSearch,      
-    //   buttonGroup
-    // },
+    components: {      
+      // basicElxTable,
+      // erplrPanel,
+      leftSearch   
+      // buttonGroup
+    },
     data () {
       return {
         buttonGroupInfo: [
